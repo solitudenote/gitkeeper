c9d3ec5344242fd51613af7ccddc6978a113d838
mrprofessor <kar.rudra008@gmail.com>
2020-02-03 04:11:21 +0530

    Support Glitch hosting

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, render_template
+import os
 import requests
 from config import config
-import os
-from urllib.parse import parse_qsl
 from flask_cors import CORS
+from urllib.parse import parse_qsl
+from exceptions import APIException
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 CORS(app)
@@ -11,7 +12,7 @@ CORS(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/authenticate/<code>", methods=["GET"])
@@ -28,6 +29,12 @@ def build_config(code):
         "client_secret": os.environ.get(config["oauth_client_secret"]),
         "code": code,
     }
+
+    # Raise exceptions if client_id or client_secret not found.
+    if not payload["client_id"]:
+        raise APIException("Client Id Not found in environment", status_code=422)
+    if not payload["client_secret"]:
+        raise APIException("Client secret Not found in environment", status_code=422)
     return url, headers, payload
 
 
@@ -36,6 +43,14 @@ def get_access_token(url, headers, payload):
     qs = dict(parse_qsl(response.text))
     creds = {item: qs[item] for item in qs}
     return creds
+
+
+# Global error handler
+@app.errorhandler(APIException)
+def handle_api_exception(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 
 if __name__ == "__main__":
